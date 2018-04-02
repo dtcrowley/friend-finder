5485278bfd8923d239c9d385433f5bdeb4ec2a63
dtcrowley <crowleyd1988@gmail.com>
2018-04-02 17:43:05 -0400

    Added API routes JS file

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -7,7 +7,33 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res){
-        var input = req.body;
-        var respones = userInput.scores;
-    })
-}
+        var userInput = req.body;
+        console.log("User input = " + JSON.stringify(userInput));
+        var userResponses = userInput.scores;
+        console.log("User responses = " + userResponses)
+
+        var matchName = "";
+        var matchImage = "";
+        var userDifference = 100;
+
+        for (var i = 0; i < friends.length; i++) {
+            var diff = 0;
+            
+            for (var x = 0; x < userResponses.length; x++) {
+                diff += Math.abs(firends[i].scores[x] - userResponses[x]);
+            }
+            console.log("Difference = " + diff);
+
+            if (diff < userDifference) {
+            
+                userDifference = diff;
+                matchName = friends[i].name;
+                matchImage = friends[i].photo;
+            }
+        }
+
+        friends.push(userInput);
+
+        res.json({status: "OK", matchName: matchName, matchImage: matchImage})
+    });
+};
