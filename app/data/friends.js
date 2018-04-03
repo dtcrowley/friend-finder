a5f83380c262ca93978955e67a8d42e6bc0c87e1
dtcrowley <crowleyd1988@gmail.com>
2018-04-03 14:41:27 -0400

    Added functionality to buttons and post to api/friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var userArray = [
     {
         name: "Ricky Bobby",
         photo: "https://ih1.redbubble.net/image.482489482.4397/flat,800x800,075,f.u2.jpg",
-        answers: [
+        scores: [
             "5",
             "4",
             "5",
@@ -18,7 +18,7 @@ var userArray = [
     {
         name: "Ron Burgundy",
         photo: "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2013/11/7/1383840401199/Ron-Burgundy-008.jpg?w=300&q=55&auto=format&usm=12&fit=max&s=72d7aa545a5ff038f0b97b9e0d554f3d",
-        answers: [
+        scores: [
             "3",
             "4",
             "3",
@@ -34,7 +34,7 @@ var userArray = [
     {
         name: "Jacobim Mugatu",
         photo: "https://ia.media-imdb.com/images/M/MV5BMTg0MTA1Njc4OV5BMl5BanBnXkFtZTgwODAyMDIyNDM@._V1_SX1777_CR0,0,1777,999_AL_.jpg",
-        answers: [
+        scores: [
             "5",
             "2",
             "1",
@@ -50,7 +50,7 @@ var userArray = [
     {
         name: "Jackie Moon",
         photo: "https://ia.media-imdb.com/images/M/MV5BMTI4OTA3OTcxNl5BMl5BanBnXkFtZTcwNzI1Njk1MQ@@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
-        answers: [
+        scores: [
             "1",
             "2",
             "1",
@@ -66,7 +66,7 @@ var userArray = [
     {
         name: "Brad Taggart",
         photo: "https://ia.media-imdb.com/images/M/MV5BMjMwMTQzMTY2Ml5BMl5BanBnXkFtZTgwNDkxMTA1NzE@._V1_SX1500_CR0,0,1500,999_AL_.jpg",
-        answers: [
+        scores: [
             "5",
             "2",
             "4",
@@ -82,7 +82,7 @@ var userArray = [
     {
         name: "Frank the Tank",
         photo: "https://ia.media-imdb.com/images/M/MV5BODM1MzU4MzYyNF5BMl5BanBnXkFtZTcwNjY4Nzc3Mw@@._V1_SY1000_CR0,0,1490,1000_AL_.jpg",
-        answers: [
+        scores: [
             "2",
             "1",
             "4",
@@ -98,7 +98,7 @@ var userArray = [
     {
         name: "Steve Butabi",
         photo: "https://ia.media-imdb.com/images/M/MV5BMjY2YTY2ODItZDBkMC00YWUwLWJhZmYtNmZiMzAwYmI1ZmE0XkEyXkFqcGdeQXVyMjk3NTUyOTc@._V1_.jpg",
-        answers: [
+        scores: [
             "1",
             "3",
             "2",
@@ -114,7 +114,7 @@ var userArray = [
     {
         name: "Chazz Michael Michaels",
         photo: "https://ia.media-imdb.com/images/M/MV5BMjIzNjkyMTA3NF5BMl5BanBnXkFtZTcwMzMyMjI0Nw@@._V1_SY1000_CR0,0,669,1000_AL_.jpg",
-        answers: [
+        scores: [
             "2",
             "5",
             "5",
@@ -130,7 +130,7 @@ var userArray = [
     {
         name: "James",
         photo: "https://ia.media-imdb.com/images/M/MV5BOTk4NDg5MTMxNl5BMl5BanBnXkFtZTgwNjAxNTE5NDE@._V1_SX1777_CR0,0,1777,960_AL_.jpg",
-        answers: [
+        scores: [
             "5",
             "3",
             "2",
@@ -146,7 +146,7 @@ var userArray = [
     {
         name: "Buddy",
         photo: "https://ia.media-imdb.com/images/M/MV5BMTU4NjcwOTU5MV5BMl5BanBnXkFtZTYwNzgxMTc3._V1_.jpg",
-        answers: [
+        scores: [
             "3",
             "3",
             "5",
